6cce8257618b390280c2212b0f650a9099285109
luciadanielasosa <a2019118793@estudiantes.upsa.edu.bo>
2020-12-08 20:00:50 -0400

    se creo la solucion Repaso 1

---
+++ Repaso1/Repaso1/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Repaso1.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Repaso1/Repaso1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Repaso1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
